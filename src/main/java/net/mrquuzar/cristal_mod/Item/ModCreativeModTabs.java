1719496663fff46569a49f373de11827172f2b8b
quuzar <solyanov.danil.2006@gmail.com>
2024-07-07 10:10:50 +0900

    One Update

---
+++ src/main/java/net/mrquuzar/cristal_mod/Item/ModCreativeModTabs.java
@@ -0,0 +1,42 @@
+package net.mrquuzar.cristal_mod.Item;
+
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.mrquuzar.cristal_mod.CristalMod;
+import net.mrquuzar.cristal_mod.block.ModBlocks;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MDOE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CristalMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> CRISTAL_TAB = CREATIVE_MDOE_TABS.register("cristal_tab",
+            () -> CreativeModeTab.builder().icon(()-> new ItemStack(ModItems.SAPPHIRE.get()))
+                    .title(Component.translatable("creativetab.cristal_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.SAPPHIRE.get());
+                        pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+
+                        pOutput.accept(ModItems.METAL_DETECTOR.get());
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                        pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+
+                    })
+                    .build());
+
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MDOE_TABS.register(eventBus);
+    };
+}
---
+++ src/main/java/net/mrquuzar/cristal_mod/Item/ModItems.java
@@ -0,0 +1,26 @@
+package net.mrquuzar.cristal_mod.Item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.mrquuzar.cristal_mod.CristalMod;
+import net.mrquuzar.cristal_mod.Item.custom.MetalDetectorItem;
+
+public class ModItems {
+    public static  final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CristalMod.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+            () -> new MetalDetectorItem(new Item.Properties().durability(100)));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
